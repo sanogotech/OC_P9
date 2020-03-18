97a1037fdee08e0f95d8e70ac66b501d291e0443
ccathala <cathala.cha@gmail.com>
2020-03-18 16:03:46 +0100

    EcritureComptableTest impl new tests
    Tests:
    -Soit 2 lignes d'écriture comptable de valeurs de crédit null, lorsque fait le
        total du crédit, alors on obtient 0. ==> getTotalCredit method
    - Soit 2 lignes d'écriture comptable de valeur de debit 80 et 20, lorsque fait
        le total du débit, alors on obtient 100. ==> getTotalDebit method
    - Soit 2 lignes d'écriture comptable de valeurs de debit null, lorsque fait le
        total du débit, alors on obtient 0. ==> getTotalDebit method
    - Soit le crédit total=100 et le débit total=100, lorsque l'on vérifie
        l'équilibre, alors on obtient true ==> isEquilibree method
    - Soit le crédit total=200 et le débit total=100, lorsque l'on vérifie
        l'équilibre, alors on obtient false ==> isEquilibree method

--- src/myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/EcritureComptableTest.java
+++ src/myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/EcritureComptableTest.java
@@ -1,20 +1,18 @@
 package com.dummy.myerp.model.bean.comptabilite;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.lang3.ObjectUtils;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -52,8 +50,9 @@ public class EcritureComptableTest {
         accountWritingList = null;
     }
 
+    // Soit 2 lignes d'écriture comptable de valeur de crédit 60 et 40, lorsque fait
+    // le total du crédit, alors on obtient 100.
     @Test
-    /* @DisplayName("Soit 2 lignes d'écriture comptable de valeur de crédit 60 et 40, lorsque fait le total du crédit, alors on obtient 100.") */
     public void Given_accountWriting1CreditIsEqual60AndAccountWriting2CreditIsEqual40_When_getTotalCreditIsUsed_Then_shouldReturn100() {
         // GIVEN
 
@@ -76,37 +75,112 @@ public class EcritureComptableTest {
 
     }
 
-    private LigneEcritureComptable createLigne(Integer pCompteComptableNumero, String pDebit, String pCredit) {
-        BigDecimal vDebit = pDebit == null ? null : new BigDecimal(pDebit);
-        BigDecimal vCredit = pCredit == null ? null : new BigDecimal(pCredit);
-        String vLibelle = ObjectUtils.defaultIfNull(vDebit, BigDecimal.ZERO)
-                                     .subtract(ObjectUtils.defaultIfNull(vCredit, BigDecimal.ZERO)).toPlainString();
-        LigneEcritureComptable vRetour = new LigneEcritureComptable(new CompteComptable(pCompteComptableNumero),
-                                                                    vLibelle,
-                                                                    vDebit, vCredit);
-        return vRetour;
-    }
-
+    // Soit 2 lignes d'écriture comptable de valeurs de crédit null, lorsque fait le
+    // total du crédit, alors on obtient 0.
     @Test
-    @Ignore
-    public void isEquilibree() {
-        EcritureComptable vEcriture;
-        vEcriture = new EcritureComptable();
+    public void Given_accountWriting1CreditIsNullAndAccountWriting2CreditIsNull_When_getTotalCreditIsUsed_Then_shouldReturn0() {
+        // GIVEN
 
-        vEcriture.setLibelle("Equilibrée");
-        vEcriture.getListLigneEcriture().add(this.createLigne(1, "200.50", null));
-        vEcriture.getListLigneEcriture().add(this.createLigne(1, "100.50", "33"));
-        vEcriture.getListLigneEcriture().add(this.createLigne(2, null, "301"));
-        vEcriture.getListLigneEcriture().add(this.createLigne(2, "40", "7"));
-        Assert.assertTrue(vEcriture.toString(), vEcriture.isEquilibree());
+        // Add account writing to list
+        accountWritingList.add(accountWriting1);
+        accountWritingList.add(accountWriting2);
 
-        vEcriture.getListLigneEcriture().clear();
-        vEcriture.setLibelle("Non équilibrée");
-        vEcriture.getListLigneEcriture().add(this.createLigne(1, "10", null));
-        vEcriture.getListLigneEcriture().add(this.createLigne(1, "20", "1"));
-        vEcriture.getListLigneEcriture().add(this.createLigne(2, null, "30"));
-        vEcriture.getListLigneEcriture().add(this.createLigne(2, "1", "2"));
-        Assert.assertFalse(vEcriture.toString(), vEcriture.isEquilibree());
+        // Set ListLigneEcriture
+        classUnderTest.getListLigneEcriture().add(accountWriting1);
+        classUnderTest.getListLigneEcriture().add(accountWriting2);
+
+        // WHEN
+        final BigDecimal result = classUnderTest.getTotalCredit();
+        // THEN
+        assertThat(result).isEqualByComparingTo("0");
+
     }
 
+    // Soit 2 lignes d'écriture comptable de valeur de debit 80 et 20, lorsque fait
+    // le total du débit, alors on obtient 100.
+    @Test
+    public void Given_accountWriting1DebitIsEqual80AndAccountWriting2DebitIsEqual20_When_getTotalDebitIsUsed_Then_shouldReturn100() {
+        // GIVEN
+
+        // Set Debit values
+        accountWriting1.setDebit(BigDecimal.valueOf(80));
+        accountWriting2.setDebit(BigDecimal.valueOf(20));
+
+        // Add account writing to list
+        accountWritingList.add(accountWriting1);
+        accountWritingList.add(accountWriting2);
+
+        // Set ListLigneEcriture
+        classUnderTest.getListLigneEcriture().add(accountWriting1);
+        classUnderTest.getListLigneEcriture().add(accountWriting2);
+
+        // WHEN
+        final BigDecimal result = classUnderTest.getTotalDebit();
+        // THEN
+        assertThat(result).isEqualByComparingTo("100");
+
+    }
+
+    // Soit 2 lignes d'écriture comptable de valeurs de debit null, lorsque fait le
+    // total du débit, alors on obtient 0.
+    @Test
+    public void Given_accountWriting1DebitIsNullAndAccountWriting2DebitIsNull_When_getTotalDebitIsUsed_Then_shouldReturn0() {
+        // GIVEN
+
+        // Add account writing to list
+        accountWritingList.add(accountWriting1);
+        accountWritingList.add(accountWriting2);
+
+        // Set ListLigneEcriture
+        classUnderTest.getListLigneEcriture().add(accountWriting1);
+        classUnderTest.getListLigneEcriture().add(accountWriting2);
+
+        // WHEN
+        final BigDecimal result = classUnderTest.getTotalDebit();
+        // THEN
+        assertThat(result).isEqualByComparingTo("0");
+
+    }
+
+    // Soit le crédit total=100 et le débit total=100, lorsque l'on vérifie
+    // l'équilibre, alors on obtient true
+    @Test
+    public void Given_totalCredit100AndTotalDebit100_When_isEquilibreeIsUsed_Then_souldReturnTrue() {
+        // GIVEN
+
+        // Init mock
+        MockitoAnnotations.initMocks(this);
+
+        // Declare mock behavior
+        when(classUnderTest.getTotalCredit()).thenReturn(BigDecimal.valueOf(100));
+        when(classUnderTest.getTotalDebit()).thenReturn(BigDecimal.valueOf(100));
+        when(classUnderTest.isEquilibree()).thenCallRealMethod();
+
+        // WHEN
+        final Boolean result = classUnderTest.isEquilibree();
+
+        // THEN
+        assertThat(result).isTrue();
+    }
+
+    // Soit le crédit total=200 et le débit total=100, lorsque l'on vérifie
+    // l'équilibre, alors on obtient false
+    @Test
+    public void Given_totalCredit200AndTotalDebit100_When_isEquilibreeIsUsed_Then_souldReturnFalse() {
+        // GIVEN
+
+        // Init mock
+        MockitoAnnotations.initMocks(this);
+
+        // Declare mock behavior
+        when(classUnderTest.getTotalCredit()).thenReturn(BigDecimal.valueOf(200));
+        when(classUnderTest.getTotalDebit()).thenReturn(BigDecimal.valueOf(100));
+        when(classUnderTest.isEquilibree()).thenCallRealMethod();
+
+        // WHEN
+        final Boolean result = classUnderTest.isEquilibree();
+
+        // THEN
+        assertThat(result).isFalse();
+    }
 }
